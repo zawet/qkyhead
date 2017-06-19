865647eb321f6e7679c6822a72926e31baf917ab
zhangwentao <zhangwentao@qtone.cn>
2017-06-19 15:20:05 +0800

    第一次提交所有代码

---
+++ qkyhead2.1.seajs/js/seaconfig2.0.js
@@ -0,0 +1,21 @@
+seajs.config({
+
+  // 别名配置
+  alias: {
+	  /***库引用***/
+	'jq':'jq/1.11.1/jquery',
+    'qkyhead': 'qkyhead2.1',//引用整体头部
+	
+  },
+
+  // 路径配置
+  paths: {
+	
+  },
+
+  // 变量配置
+  vars: {
+    'locale': 'zh-cn'
+  }
+});
+
